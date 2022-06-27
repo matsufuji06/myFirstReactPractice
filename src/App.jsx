58e74c3abbd609280639e864f85f575c1bbee9e2
Naoya Matsufuji <n.matsufuji06@gmail.com>
2022-06-28 08:10:53 +0900

    export, inportでコンポーネント化

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>こんにちは！</h1>
+      <p>こんにちは！</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
