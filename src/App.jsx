9409983899790cc8ea91c894efed6dcf1a32f5a4
Naoya Matsufuji <n.matsufuji06@gmail.com>
2022-06-29 07:46:02 +0900

    スタイル属性やクリックイベントの埋め込み

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <React.Fragment>
-      <h1>こんにちは！</h1>
-      <p>こんにちは！</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <p style={contentStyle}>お元気ですわ。</p>
+      <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
   );
 };
