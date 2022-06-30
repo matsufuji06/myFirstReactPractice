1597f69a3da1f2bd88d494bcbe152e034b9e0629
Naoya Matsufuji <n.matsufuji06@gmail.com>
2022-06-30 11:49:13 +0900

    再レンダリングで３の倍数の時に顔文字を表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
-  const [num, setNum] = useState(0);
+  const onClickShowSwitchFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  // 第二引数の中身にだけ関心を持つようになる
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
 
   return (
     <React.Fragment>
@@ -14,7 +29,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">お元気ですわ。</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickShowSwitchFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^_-)-☆</p>}
     </React.Fragment>
   );
 };
